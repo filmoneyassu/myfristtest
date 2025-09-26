80c1ee09564c25f50d10b9bded6c7d471b0c2069
Filmon Eyassu <filmonaeyasu@yahoo.com>
2025-09-26 18:32:33 +0200

    sort small nr and big nr

--- challenge.js
+++ challenge.js
@@ -17,9 +17,63 @@ triArea(3, 2)
 triArea(7, 4)
 
 triArea(10, 10)
+
 //Notes
 
 /*The area of a triangle is: (base * height) / 2
 Don't forget to return the result.
 If you get stuck on a challenge, find help in the Resources tab.
 If you're really stuck, unlock solutions in the Solutions tab.*/
+
+
+
+
+
+
+function howManySeconds(hours) {
+  const secondsPerHour = 3600;
+  console.log(hours.map(h => h * secondsPerHour));
+
+  return ""
+
+}
+
+console.log(howManySeconds([1]))
+
+
+
+function parameter(a, b) {
+
+    console.log(a % b);
+    
+    
+}
+
+parameter(10, 4)
+
+
+
+let num = [2, 4, 1, 3, 5]
+
+function minMax() {
+
+    let num2 = num.sort((a, b) => a - b); 
+    let num3 = [num2[0], num2.length]
+
+
+
+
+  
+    
+
+
+    console.log(num3);
+
+
+    
+} 
+
+
+
+minMax()
+
