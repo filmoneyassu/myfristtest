5cd5eaffcf5ca5b3bf747b3f3f50a9dbdeb47e9d
Filmon Eyassu <filmonaeyasu@yahoo.com>
2025-09-26 10:36:29 +0200

    first commit

---
+++ Burrrrrrp.js
@@ -0,0 +1,15 @@
+function longBurp2(num) {
+    let message = "Bu"
+  for (let i = 0; i < num; i++) {
+
+    message += "r"
+
+  }
+
+  console.log(message + "p");
+  
+}
+
+longBurp2(3)
+longBurp2(5)
+longBurp2(9)
